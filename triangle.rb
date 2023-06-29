54ddcfd3af836cc90dc9eee5a2a169908f39fe19
Mason <masonlynass@gmail.com>
2023-06-29 13:09:37 -0700

    triangle problem 1 passed

--- triangle.rb
+++ triangle.rb
@@ -14,9 +14,15 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == b && b == c 
+    return :equilateral
+  end
+  if a != b && b != c && c != a 
+    return :scalene
+  end
+  return :isosceles
 end
 
 # Error class used in part 2.  No need to change this code.
-class TriangleError < StandardError
-end
+# class TriangleError < StandardError
+# end
